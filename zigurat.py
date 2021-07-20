5a76df999b90b24412f8cfc6e605391eac9b02ad
Ricardo Ruiz <ricardoruizfdez@gmail.com>
2021-07-20 20:15:44 +0200

    feat: allow executing nc playlists of any user
    nc: non-collaborative

--- zigurat.py
+++ zigurat.py
@@ -1,4 +1,5 @@
 import os
+import time
 from pprint import pprint
 from sys import exit
 
@@ -9,16 +10,19 @@ CLIENT_ID = os.environ.get("SPOTIPY_CLIENT_ID")
 CLIENT_SECRET = os.environ.get("SPOTIPY_CLIENT_SECRET")
 REDIRECT_URI = os.environ.get("SPOTIPY_REDIRECT_URI")
 
-user = "riki319"
+user_auth = "riki319"
+user_program = "riki319"
+
 program_name = "Turing Machine - Palindrome"
 
+scope = "user-modify-playback-state playlist-modify-public user-read-playback-state"
 sp = spotipy.Spotify(
     auth_manager=SpotifyOAuth(
-        username=user,
+        username=user_auth,
         client_id=CLIENT_ID,
         client_secret=CLIENT_SECRET,
         redirect_uri=REDIRECT_URI,
-        scope="streaming",
+        scope=scope,
     )
 )
 
@@ -30,10 +34,9 @@ computer_device = next(
 if not computer_device["is_active"]:
     sp.transfer_playback(computer_device["id"])
 
-playlists = sp.user_playlists(user)
+playlists = sp.user_playlists(user_program)
 program = next(pl for pl in playlists["items"] if pl["name"] == program_name)
 word = program["description"]
-
 instructions = [
     {"index": i, "name": item["track"]["name"], "uri": item["track"]["uri"]}
     for i, item in enumerate(sp.playlist_tracks(program["id"])["items"])
@@ -42,22 +45,27 @@ instructions = [
 reader_head = 0
 accepted = False
 
+no_goto_instructions = (
+    "Delete",
+    "Left",
+    "Right",
+    "Start Again",
+    "Right to End",
+)
+reader_head = 0
+t = 3
+
 current_instruction = instructions[0]
 halt = (
     "Yes" in current_instruction["name"] or "No" in current_instruction["name"]
 )
 
 while not halt:
+    print(word)
+    print(current_instruction["name"])
     sp.add_to_queue(current_instruction["uri"])
-
-    if "Yes" in current_instruction["name"]:
-        accepted = True
-        break
-    elif "No" in current_instruction["name"]:
-        break
-
-    next_instruction_index = current_instruction["index"] + 1
-    current_instruction = instructions[next_instruction_index]
+    time.sleep(t)
+    sp.next_track()
 
     if current_instruction["name"] == "If":
         next_index = current_instruction["index"] + 1
@@ -90,48 +98,71 @@ while not halt:
             next_instruction_index = current_instruction["index"] + 4
             current_instruction = instructions[next_instruction_index]
 
-    elif current_instruction["name"] == "Delete":
-        word[reader_head] = "#"
-        sp.user_playlist_change_details(user, program["id"], description=word)
-
-    elif current_instruction["name"] == "Left":
-        reader_head -= 1
-
-    elif current_instruction["name"] == "Right":
-        reader_head += 1
-
-    elif current_instruction["name"] == "Start Again":
-        reader_head = -1
-
-    elif current_instruction["name"] == "Right to End":
-        reader_head = len(word)
-
     elif current_instruction["name"] == "Goto":
         line_number_index = current_instruction["index"] + 1
         line_number = instructions[line_number_index]
 
         sp.add_to_queue(line_number["uri"])
 
+        if line_number["name"] == "Zero":
+            line_number["name"] = "0"
+        elif line_number["name"] == "One":
+            line_number["name"] = "1"
+
         current_instruction = instructions[int(line_number["name"])]
+
+        sp.add_to_queue(line_number["uri"])
+        time.sleep(t)
+        sp.next_track()
+
+        current_instruction = instructions[int(line_number["name"])]
+
+    elif current_instruction["name"] in no_goto_instructions:
+
+        if current_instruction["name"] == "Delete":
+            word = word[:reader_head] + "#" + word[reader_head + 1 :]
+            if user_auth == user_program:
+                sp.user_playlist_change_details(
+                    user_program, program["id"], description=word
+                )
+
+        elif current_instruction["name"] == "Left":
+            reader_head -= 1
+
+        elif current_instruction["name"] == "Right":
+            reader_head += 1
+
+        elif current_instruction["name"] == "Start Again":
+            reader_head = len(word) - len(word.lstrip("#")) - 1
+
+        elif current_instruction["name"] == "Right to End":
+            reader_head = len(word.rstrip("#"))
+
+        next_instruction_index = current_instruction["index"] + 1
+        current_instruction = instructions[next_instruction_index]
 
     halt = (
         "Yes" in current_instruction["name"]
         or "No" in current_instruction["name"]
     )
 
-if accepted:
-    print(f"La palabra {word} ha sido aceptada por {program_name}")
-else:
-    print(f"La palabra {word} ha sido rechazada por {program_name}")
-
 
 sp.start_playback()
 
+sp.add_to_queue(current_instruction["uri"])
+time.sleep(t)
+sp.next_track()
+
+if "Yes" in current_instruction["name"]:
+    print(f"La palabra {word} ha sido aceptada por {program_name}")
+elif "No" in current_instruction["name"]:
+    print(f"La palabra {word} ha sido rechazada por {program_name}")
+
 # TODO: Manage playlist pagination.
 # while playlists:
-# for i, playlist in enumerate(playlists['items']):
-#   print(f"\t{i + 1 + playlists['offset']} {playlist['uri']} {playlist['name']}")
-#   if playlists['next']:
-#       playlists = sp.next(playlists)
-#   else:
-#       playlists = None
+#   for i, playlist in enumerate(playlists["items"]):
+#       print(f"\t{i + 1 + playlists["offset"]} {playlist["uri"]} {playlist["name"]}")
+#       if playlists["next"]:
+#           playlists = sp.next(playlists)
+#       else:
+#           playlists = None
